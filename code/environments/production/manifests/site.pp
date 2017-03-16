ee40775683742fe62fdb41de783fb39c76e5a214
Chris Waring <chrisw@idxbroker.com>
2017-03-16 09:17:56 -0700

    Testing hiera mote::message
    Applying hire motel::message to nodes 1 2

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -5,10 +5,12 @@ node default {
 node 'pagent1.idx.local' {
     include documents
     include users
+
+    notify { hiera(motd::message): }
 }
 
 node 'puppetagent2.idxlocal' {
-
+    notify { hiera(motd::message): }
 }
 
 node 'desktop-spare1.idx.local' {
