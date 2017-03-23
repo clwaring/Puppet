03115e2794dc6e315bcc8e1f2f938adc279a7c82
Chris Waring <chrisw@idxbroker.com>
2017-03-23 16:31:16 -0700

    Choco working
    Adding all the things that used to be installed with Ninite

--- code/environments/production/manifests/site.pp
+++ code/environments/production/manifests/site.pp
@@ -18,6 +18,56 @@ node 'desktop-spare1.idx.local' {
         ensure          => latest,
         provider        => 'chocolatey',
     }
+
+    package { 'winrar':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
+
+    package { 'googlechrome':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
+
+    package { 'firefox':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
+
+    package { 'jre8':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
+
+    package { 'jdk8':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
+
+    package { 'adobereader':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
+
+    package { 'notepadplusplus':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
+
+    package { 'putty':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
+
+    package { 'malwarebytes':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
+
+    package { 'winscp':
+        ensure          => latest,
+        provider        => 'chocolatey',
+    }
 }
 
 node 'idxs-mac-mini.local' {
