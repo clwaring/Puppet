b394427f317d7745d8fb06181bf0352f1ca1117e
Chris Waring <chrisw@idxbroker.com>
2017-04-06 14:08:28 -0700

    More syntax for Vera package
    ..

--- code/environments/production/modules/software/manifests/init.pp
+++ code/environments/production/modules/software/manifests/init.pp
@@ -105,9 +105,7 @@ class software::ubuntu {
   package { 'veracrypt':
       ensure          => latest,
       provider        => hiera(provider),
-      require         => Exec['add-apt-repository ppa:unit193/encryption'],
-      require         => Exec['apt update'],
-      require         => Exec['apt install veracrypt'],
+      require         => Exec['add-apt-repository ppa:unit193/encryption', 'apt update', 'apt install veracrypt'],
   }
 
   package { 'vim':
